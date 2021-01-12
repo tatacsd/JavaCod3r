0f61f3768708340fab4abb4c7db6d0ef6fa7fa2a
Thays <thayscasado@hotmail.com>
2021-01-12 00:37:11 -0800

    16. Comentários

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,9 +1,21 @@
 package fundamentos;
 
+/**
+ * Java Doc (explicação)
+ * 
+ * @autor Thays
+ */
 public class PrimeiroPrograma {
+	
 	public static void main(String[] args) {
 		System.out.println("Primeiro Programa! Parte #01");	
 		System.out.println("Primeiro Programa! Parte #02");		
+		
+		
+		/*
+		 * linha 1
+		 * linha 2
+		 */
 
 	}
 
