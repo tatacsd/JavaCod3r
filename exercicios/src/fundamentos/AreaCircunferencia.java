0cad4505963f49203ef07c6430c5b9e6e80a076b
Thays <thayscasado@hotmail.com>
2021-01-12 00:44:57 -0800

    17. Variáveis e Constantes

---
+++ exercicios/src/fundamentos/AreaCircunferencia.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class AreaCircunferencia {
+	public static void main(String[] args) {
+	//	System.out.println(2+3);
+		double raio = 3.4;
+		final double PI = 3.14159; // final = constante
+		
+		double area = PI * raio * raio;
+		
+		System.out.println("Area = " + area + "m2.");
+		
+	}
+}
