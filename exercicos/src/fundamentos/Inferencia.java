57b12c7d601a7957cec0a0049240ba53ac4e04f0
Thays <thayscasado@hotmail.com>
2021-01-18 23:22:55 -0800

    20. Inferência de Tipos

---
+++ exercicos/src/fundamentos/Inferencia.java
@@ -0,0 +1,25 @@
+package fundamentos;
+
+public class Inferencia {
+
+	public static void main(String[] args) {
+		
+		double a = 4.5;
+		System.out.println(a);
+		
+		// Java vai inferir de acordo com o valor colocado
+		var b = 4.5;
+		
+		
+		double d;
+		d = 123;
+		System.out.println(d);
+		
+		// wrong
+		// var e;
+		// e = 123;
+		
+
+	}
+
+}
