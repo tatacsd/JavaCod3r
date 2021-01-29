968c97283e1b18896b9e34798d478422cc427ba8
Thays <thayscasado@hotmail.com>
2021-01-29 03:31:35 -0800

    38. Desafio Operadores Aritmeticos

---
+++ exercicos/src/fundamentos/DesafioOperadorAritmetico.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class DesafioOperadorAritmetico {
+	public static void main(String[] args) {
+		double part1 = Math.pow(6 * (3 + 2), 2) / (3 * 2); 
+		double part2 = Math.pow((1 - 5) * (2 - 7) / 2, 2);
+		double part3 = Math.pow(part1 - part2, 3);
+		double part4 = Math.pow(10, 3);
+		int finalScore = (int) (part3 / part4);
+		
+		System.out.println(finalScore);
+	}
+
+}
