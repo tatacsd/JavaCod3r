4132a40b7c66aa26409610e73721ce8783f5b4a4
Thays <thayscasado@hotmail.com>
2021-02-03 22:31:49 -0800

    64. While#01

---
+++ exercicos/src/estruturaDeControle/WhileDeterminado.java
@@ -0,0 +1,14 @@
+package estruturaDeControle;
+
+public class WhileDeterminado {
+	public static void main(String[] args) {
+		
+		int i = 0;
+		while(i <= 20) {
+			System.out.printf("i = %d\n", i);
+			i += 2;
+		}
+		
+	}
+
+}
