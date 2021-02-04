b66da9831094c7313c0ddc37f692c657e95f328b
Thays <thayscasado@hotmail.com>
2021-02-03 22:03:55 -0800

    58. Desafio IF

---
+++ exercicos/src/estruturaDeControle/DesafioIf.java
@@ -0,0 +1,13 @@
+package estruturaDeControle;
+
+public class DesafioIf {
+	public static void main(String[] args) {
+		double nota = 1.3;
+		
+		if(nota >= 9.0); { // Ponto e vírgula
+			System.out.println("Quandro Honra!!");
+			System.out.println("Você é fera!!");
+		}
+	}
+
+}
